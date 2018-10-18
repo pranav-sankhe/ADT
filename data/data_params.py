482065f52ab4d8b0f1e224544442dee17110aab4
Pranav <pranavsankhe40@gmail.com>
2018-10-18 05:33:21 +0530

    parameters for data utils file

---
+++ data/data_params.py
@@ -0,0 +1,10 @@
+data_dir = 'SMT_DRUMS'
+num_drums = 3
+drums = ['HH', 'KD', 'SD', 'MIX']
+num_gen_type = 3
+gen_type = ['RealDrum', 'TechnoDrum_02', 'WaveDrum_02']
+
+max_audio_length = 1323000
+sample_rate = 44100
+
+test_filepath = 'test.wav'
