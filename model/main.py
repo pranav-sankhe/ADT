9dc026ab1c4860155e5752fca2cb181a39c19354
Pranav <pranavsankhe40@gmail.com>
2018-11-02 15:21:14 +0530

    fmeasure of around 0.82 obtained

--- model/main.py
+++ model/main.py
@@ -1,13 +1,45 @@
 import numpy as np 
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import sys 
 sys.path.insert(0, '../data')
 import data_utils
 import hparams
 import utils
+#------------------------------------------------------------------------------------------------------------------------------------
+# utils.get_templates()
+#------------------------------------------------------------------------------------------------------------------------------------
 
 
-utils.get_templates()
+#------------------------------------------------------------------------------------------------------------------------------------
+gen_type = hparams.gen_type
+drum_type_index = 3         # select 'MIX' 
+data_dir = hparams.data_dir
+gen_type_index = 0
+
+audio_file_list = data_utils.get_audio_files(data_dir, drum_type_index, gen_type_index)
+
+audio_file_list = [x.split('.')[0] for x in audio_file_list]
+
+audio_file_list = audio_file_list[0:int(hparams.get_template_length*len(audio_file_list))]
+file_list_length = len(audio_file_list)
+
+xml_file_list = data_utils.get_xml_files(data_dir, drum_type_index, gen_type_index)
+xml_file_list = [x.split('.')[0] for x in xml_file_list]
+xml_file_list = np.intersect1d(xml_file_list, audio_file_list)
+
+audio_file_names = [x.split('#')[0] for x in audio_file_list]
+
+f_measure = []
+
+for file in audio_file_list:
+	f_measure.append(utils.eval(file))
+
+print('F-measure = ', np.mean(f_measure))
+#------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+#------------------------------------------------------------------------------------------------------------------------------------
 # pred_activations = utils.predict_activations(filename)
 # plt.subplot(311)
 # plt.plot(pred_activations[0,:])
@@ -15,9 +47,16 @@ utils.get_templates()
 # plt.plot(pred_activations[1,:])
 # plt.subplot(313)
 # plt.plot(pred_activations[2,:])
-# filename = audio_file_list[0]
-# utils.eval(filename)
+# plt.show()
+#------------------------------------------------------------------------------------------------------------------------------------
 
+
+
+
+
+
+
+#------------------------------------------------------------------------------------------------------------------------------------
 # templates = np.load('templates.npy')
 # HH_templates = templates[:,0]
 # KD_templates = templates[:,1]
@@ -28,4 +67,4 @@ utils.get_templates()
 # plt.plot(KD_templates); plt.legend(['KD'])
 # plt.figure()
 # plt.plot(SD_templates); plt.legend(['SD'])
-# plt.show()
+#------------------------------------------------------------------------------------------------------------------------------------
